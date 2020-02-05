df06a46c7e895930d31d1272a71b4ab277be8462
sergisalnic123 <sergi1397@gmail.com>
2020-02-05 17:12:25 +0100

    commit 0
    empezamos
    
    - Gitignore

---
+++ Piritis/Assets/Scripts/CartaObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/CartaObject", order = 1)]
+
+public class CartaObject : MonoBehaviour
+{
+
+    public enum TipoPasiva
+    {
+
+    }
+
+    public string NombreCarta;
+    public Sprite ImagenCarta;
+    public int levelCarta;
+    public int rolValor;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
