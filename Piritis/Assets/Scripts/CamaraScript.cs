73fd8b099e6338bb420cc72b37662d5c23d32060
davidMolinaBastos <dmolinab@edu.tecnocampus.cat>
2020-02-10 16:27:46 +0100

    Cámara implementada

---
+++ Piritis/Assets/Scripts/CamaraScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraScript : MonoBehaviour
+{
+    public GameObject target;
+    private void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player");
+    }
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, -1);
+    }
+}
